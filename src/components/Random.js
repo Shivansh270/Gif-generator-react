b1e140f2767fd173d717c7f1652f34cbe51a0a74
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-06-19 23:55:47 +0530

    feature- custom hook created

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,49 +1,36 @@
-
+import React, { useState } from 'react'
 import axios from 'axios';
-import React, { useState, useEffect } from 'react'
-import Spinner from './Spinner'
+import { useEffect } from 'react';
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
 
-  const[gif,setGif] = useState('');
 
-  const[loading,setLoading] = useState(false);
+  const {gif, loading, fetchData} = useGif();
 
 
-  async function fetchData() {
-    setLoading(true)
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-
-    const {data} = await axios.get(url);
-    console.log(data)
-    const setImage = data.data.images.downsized_large.url;
-    setGif(setImage)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-  
-
-  function clickHandler(){
-    fetchData();
-  }
- 
   return (
-    <div className='w-1/2 bg-green-600 rounded-lg
-    border-black flex flex-col items-center gap-5 mt-[25px]'>
+    <div className='w-1/2  bg-green-500 rounded-lg border border-black
+    flex flex-col items-center gap-y-5 mt-[15px]'>
+
+      <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> A Random Gif</h1>
+
+    {
+        loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
+    }
+
       
-      <h1 className='text-2xl uppercase underline'>random gif</h1>
 
-      {
-        loading ? (<Spinner />) : ( <img src={gif} width="450" />)
-      }
+      <button onClick={() => fetchData()}
+      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
 
-      <button className='w-10/12 bg-orange-400 text-lg py-2 rounded-lg mb-[20px]' onClick={clickHandler}>generate</button>
+       Generate
+
+      </button>
 
     </div>
   )
